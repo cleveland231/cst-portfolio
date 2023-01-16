24e47e3aad6c9d7036d92003283c546ed73790c7
Cleveland Ticoalu <ayocleveland@gmail.com>
2023-01-15 23:35:07 -0700

    Styling welcome component

--- src/Components/Welcome/Welcome.tsx
+++ src/Components/Welcome/Welcome.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import './Welcome.css';
 
 const Welcome = () => {
   return (
     <div className='Welcome'>
+      <div className='WelcomeBox'>
+      </div>
+
+      <div className='WelcomeBox'>
         Hello friend! My name is Cleveland Ticoalu and I am a freelance software engineer. I just graduated from Turing School of 
         Software and Design with a focus in Front-End development. Prior to studying at Turing I worked in health care and 
-        in oil and energy. These busy environments taught me how to quickly adapt and communicate on different teams. Eventually 
-        I realized that I would rather apply my intellect to the world of software development. This new trajectory has been 
+        in oil and energy. These busy environments taught me how to quickly adapt and communicate on different teams. This new trajectory has been 
         immensely rewarding and I look forward to broadening and sharpening my skill set in the future.
+      </div>
     </div>
   )
 }
